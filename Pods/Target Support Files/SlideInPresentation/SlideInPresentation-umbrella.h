b413d8ade0ff26f4509cb1cf2a9fa82e417bf64c
sunyazhou <sunyazhou@kingsoft.com>
2017-10-31 14:55:33 +0800

    1
    1

---
+++ Pods/Target Support Files/SlideInPresentation/SlideInPresentation-umbrella.h
@@ -0,0 +1,20 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "ScreeRotateViewController.h"
+#import "SlideInPresentationAnimator.h"
+#import "SlideInPresentationController.h"
+#import "SlideInPresentationManager.h"
+
+FOUNDATION_EXPORT double SlideInPresentationVersionNumber;
+FOUNDATION_EXPORT const unsigned char SlideInPresentationVersionString[];
+
